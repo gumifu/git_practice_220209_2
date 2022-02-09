0587a805ecacbc3652775ed5889f59d7aaa7f53f
gumifu <ffumi.66@gmail.com>
2022-02-09 23:18:00 +0900

    added funcC-2

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          func C-1 Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+          func C-2 Welcome to <a href="https://nextjs.org">Next.js!</a>
 
         <p className={styles.description}>
           Get started by editing{' '}
